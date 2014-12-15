241bb1f51cdb5ec7b2c6f0979a20ae54400b58d4
Junyang Ng <juny4ng@gmail.com>
2014-12-15 20:10:37 +0800

    Add create directory

--- rails/recipes/dotenv.rb
+++ rails/recipes/dotenv.rb
@@ -15,6 +15,18 @@ require 'shellwords'
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
 
+  Chef::Log.info("Creating directory..")
+
+
+  directory "#{deploy[:deploy_to]}/shared" do
+      owner "deploy"
+      group "www-data"
+      mode 0774
+      recursive true
+      action :create
+  end
+
+
   Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
 
   begin
