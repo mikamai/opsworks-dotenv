1ab63f93f1d26c948ef4d8a8c8d83ff614c7783c
Junyang Ng <juny4ng@gmail.com>
2014-12-15 22:33:30 +0800

    Remove create dir

--- rails/recipes/dotenv.rb
+++ rails/recipes/dotenv.rb
@@ -15,16 +15,6 @@ require 'shellwords'
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
 
-  Chef::Log.info("Rails::DotEnv -> Creating directory.. #{deploy[:deploy_to]}/shared/config")
-
-  directory "#{deploy[:deploy_to]}/shared/config" do
-      owner "deploy"
-      group "www-data"
-      mode 0774
-      recursive true
-      action :create
-  end
-
   Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
 
   begin
