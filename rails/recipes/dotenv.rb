0a6082c20bd5d7dd63cf67a9be2d84be505c50d1
Junyang Ng <juny4ng@gmail.com>
2014-12-15 17:47:22 +0800

    Add begin try around file open

--- rails/recipes/dotenv.rb
+++ rails/recipes/dotenv.rb
@@ -17,11 +17,16 @@ node[:deploy].each do |application, deploy|
 
   Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
 
-  open("#{deploy[:deploy_to]}/shared/.env", 'w') do |f|
-    require 'yaml'
-    deploy[:app_env].to_h.each do |name, value|
-      f.puts "#{name}=#{value.to_s}"
+  begin
+    open("#{deploy[:deploy_to]}/shared/.env", 'w') do |f|
+      require 'yaml'
+      deploy[:app_env].to_h.each do |name, value|
+        f.puts "#{name}=#{value.to_s}"
+      end
     end
+  rescue => e
+    Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env} failed")
+    Chef::Log.info("#{e.message}")
   end  
 end
 
