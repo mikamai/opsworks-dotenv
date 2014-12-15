aa2a1fa6f8e1007a64e0b17b0cb84f885058d6be
Junyang Ng <juny4ng@gmail.com>
2014-12-15 21:19:08 +0800

    Add create directory

--- rails/recipes/dotenv.rb
+++ rails/recipes/dotenv.rb
@@ -15,10 +15,9 @@ require 'shellwords'
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
 
-  Chef::Log.info("Creating directory..")
+  Chef::Log.info("Rails::DotEnv -> Creating directory.. #{deploy[:deploy_to]}/shared/config")
 
-
-  directory "#{deploy[:deploy_to]}/shared" do
+  directory "#{deploy[:deploy_to]}/shared/config" do
       owner "deploy"
       group "www-data"
       mode 0774
@@ -26,13 +25,11 @@ node[:deploy].each do |application, deploy|
       action :create
   end
 
-
   Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
 
   begin
     Chef::Log.info("#{deploy[:deploy_to]}")
-    Chef::Log.info(File.exists?("#{deploy[:deploy_to]}/shared/"))
-    open("#{deploy[:deploy_to]}/shared/.env", 'w') do |f|
+    open("#{deploy[:deploy_to]}/shared/config/.env", 'w') do |f|
       require 'yaml'
       deploy[:app_env].to_h.each do |name, value|
         f.puts "#{name}=#{value.to_s}"
@@ -43,5 +40,3 @@ node[:deploy].each do |application, deploy|
     Chef::Log.info("#{e.message}")
   end  
 end
-
-
