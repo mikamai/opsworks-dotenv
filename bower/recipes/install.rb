006681a38411008ad8c08b8feb0f8e27ff89b6b5
arnvald <arnvald.to@gmail.com>
2015-01-27 17:12:18 +0800

    add bower script

---
+++ bower/recipes/install.rb
@@ -0,0 +1,14 @@
+node[:deploy].each do |_, deploy|
+  script "install_bower" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code "npm install -g bower"
+  end
+
+  execute "bower install" do
+    cwd "#{deploy[:deploy_to]}/current/"
+    user "deploy"
+    environment ({'HOME' => '/home/deploy'})
+  end
+end
