ef191e48a12288b72e8c50a58ecc97dcc322e32f
Junyang Ng <juny4ng@gmail.com>
2015-02-21 13:52:52 +0800

    Add env

--- whenever/recipes/default.rb
+++ whenever/recipes/default.rb
@@ -5,7 +5,7 @@ node[:deploy].each do |application, deploy|
     layers = node[:opsworks][:instance][:layers]
     cwd "#{deploy[:deploy_to]}/current"
     user 'deploy'
-    code "bundle exec whenever --set environment=#{deploy[:rails_env]} --update-crontab #{application} --roles #{layers.join(',')}"
+    code "RAILS_ENV=#{deploy[:rails_env]} bundle exec whenever --set environment=#{deploy[:rails_env]} --update-crontab #{application} --roles #{layers.join(',')}"
     only_if "cd #{deploy[:deploy_to]}/current && bundle show whenever"
   end
 end
