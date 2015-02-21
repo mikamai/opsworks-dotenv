9ddac9387fb58e8f72da3e91e710b56758561f9e
Junyang Ng <juny4ng@gmail.com>
2015-02-21 18:32:43 +0800

    Remove cwd for bower install

--- bower/recipes/install.rb
+++ bower/recipes/install.rb
@@ -2,7 +2,6 @@ node[:deploy].each do |_, deploy|
   script "install_bower" do
     interpreter "bash"
     user "root"
-    cwd "#{deploy[:deploy_to]}/current"
     code "npm install -g bower"
   end
 end
