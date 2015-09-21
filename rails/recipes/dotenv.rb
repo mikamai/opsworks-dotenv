bcc6da6de9bd71801a819a5a9ed4c761ba0e6301
Connor Warnock <connorwarnock@gmail.com>
2015-09-21 11:00:41 -0700

    Include app env variables
    This includes all env vars set in 'Apps' as well as custom json in a Layer.

--- rails/recipes/dotenv.rb
+++ rails/recipes/dotenv.rb
@@ -16,13 +16,12 @@ node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
 
   Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
-
+  
+  environment_variables = deploy[:app_env].to_h.merge(deploy[:environment_variables].to_h)
   open("#{deploy[:deploy_to]}/shared/.env", 'w') do |f|
     require 'yaml'
-    deploy[:app_env].to_h.each do |name, value|
+    environment_variables.each do |name, value|
       f.puts "#{name}=#{value.to_s.shellescape}"
     end
   end  
 end
-
-
