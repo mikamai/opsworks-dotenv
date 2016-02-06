337d9fa125631c4c2f4ab937accefe0b1d970f88
Melvrick Goh <melvrickgoh@hotmail.com>
2016-02-06 13:21:54 +0800

    add languages

---
+++ whenever/recipes/languages.rb
@@ -0,0 +1,17 @@
+log "installing ttf-wqy-zenhei & ttf-wqy-microhei"
+
+node[:deploy].each do |application, deploy|
+  execute 'apt-get install ttf-wqy-zenhei' do
+    command 'apt-get install ttf-wqy-zenhei'
+    ignore_failure true
+    only_if { apt_installed? }
+    action :nothing
+  end
+
+  execute 'apt-get install ttf-wqy-microhei' do
+    command 'apt-get install ttf-wqy-microhei'
+    ignore_failure true
+    only_if { apt_installed? }
+    action :nothing
+  end
+end
